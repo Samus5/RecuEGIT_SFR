276ecd3fce9f37e72b640dc53fc24b4ae9416768
Samu <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2024-02-14 10:04:26 +0100

    Final Calculadora

--- CalculadoraEgit/src/ed/recuperacionegit/Calculadora.java
+++ CalculadoraEgit/src/ed/recuperacionegit/Calculadora.java
@@ -30,14 +30,39 @@ public static void resta() {
 		System.out.println("1. Suma");
 		System.out.println("2. Resta");
 		int opcion = sc.nextInt();
-	
+	if(opcion <= 0 && opcion > 4){
+		throw new IllegalArgumentException();
+	}
 	if(opcion == 1) {
 	suma();
 	}
 	if(opcion == 2) {
 		resta();
 	}
-	
+	if(opcion == 3) {
+		dividir();
+	}
+	if(opcion == 4) {
+		multiplicar();
+	}
+	}
+	private static void multiplicar() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca un numero");
+		Double numero1 = sc.nextDouble();
+		System.out.println("Introduzca el segundo numero");
+		Double numero2 = sc.nextDouble();
+		Double mult = numero1 * numero2;
+		System.out.println("El resultado es " +mult);
+	}
+	private static void dividir() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca un numero");
+		Double numero1 = sc.nextDouble();
+		System.out.println("Introduzca el segundo numero");
+		Double numero2 = sc.nextDouble();
+		Double div = numero1 / numero2;
+		System.out.println("El resultado es " +div);
 	}
 
 }
